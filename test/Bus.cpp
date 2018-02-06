837eb350742df258416db60dee1cfeb45638418a
TENMYO Masakazu <tenmyo@gmail.com>
2018-02-06 23:58:26 +0900

    test(Bus16): Extract the test fixture class

--- test/Bus.cpp
+++ test/Bus.cpp
@@ -12,331 +12,231 @@
 // System headers
 #include <cstring> // memset, memcmp
 
-using namespace nes_emu;
-namespace {
-uint_fast16_t gaddr;
-BusAccessKind gop;
-void errorCallback(uint_fast16_t addr, BusAccessKind op) {
-  gaddr = addr;
-  gop = op;
-}
-} // namespace
+namespace nes_emu {
 
-TEST(Bus16Test, MapToExists) {
+namespace {
+class Bus16Test : public ::testing::Test {
+protected:
+  virtual void SetUp() override {
+    this->p1_ = this->sram1_.data();
+    this->p2_ = this->sram2_.data();
+    memset(this->p1_, 0x01, this->sram1_.size());
+    memset(this->p2_, 0x23, this->sram2_.size());
+  }
+  virtual void TearDown() override {}
+  Bus16 bus_{[&](Bus16::AddressType addr, BusAccessKind op) -> void {
+    ++this->cnt_;
+    this->addr_ = addr;
+    this->op_ = op;
+  }};
+  Bus16::AddressType addr_ = 0;
+  BusAccessKind op_ = BusAccessKind::kNone;
+  int cnt_ = 0;
+  Sram<0x400> sram1_;
+  Sram<0x400> sram2_;
+  uint8_t *p1_;
+  uint8_t *p2_;
+};
+} // namespace
+#define EXPECT_BUS_ERROR(cnt, addr, op)                                        \
+  EXPECT_EQ(this->cnt_, cnt);                                                  \
+  EXPECT_EQ(this->addr_, addr);                                                \
+  EXPECT_EQ(this->op_, op);
+
+TEST_F(Bus16Test, MapToExists) {
   // Setup
-  Bus16 bus(errorCallback);
-  Sram<1024> sram;
-  ASSERT_FALSE(sram.map(&bus, 0));
+  ASSERT_FALSE(this->sram1_.map(&this->bus_, 0));
   // Do
-  auto ret = sram.map(&bus, 0);
+  auto ret = this->sram1_.map(&this->bus_, 0);
   // Verify
   EXPECT_TRUE(ret);
   EXPECT_EQ(ret.value(), std::errc::file_exists);
 }
-TEST(Bus16Test, MapToMiddle) {
+TEST_F(Bus16Test, MapToMiddle) {
   // Setup
-  Bus16 bus(errorCallback);
-  Sram<512> sram;
-  auto p = sram.data();
-  memset(p, 0xce, sram.size());
-  ASSERT_FALSE(sram.map(&bus, 256));
-  uint8_t buf[1 * 1024] = {0};
-  uint_fast16_t addr = 256;
-  uint_fast16_t bytes = 511;
-  gaddr = 0;
-  gop = BusAccessKind::kNone;
+  ASSERT_FALSE(this->sram1_.map(&this->bus_, 0x100));
+  uint8_t buf[0x400 + 1] = {0};
+  uint_fast16_t addr = 0x100;
+  uint_fast16_t bytes = this->sram1_.size();
   // Do
-  bus.read(addr, bytes, buf);
+  this->bus_.read(addr, bytes, buf);
   // Verify
-  EXPECT_EQ(gaddr, 0);
-  EXPECT_EQ(gop, BusAccessKind::kNone);
-  EXPECT_EQ(std::memcmp(buf, p, bytes), 0);
-  EXPECT_NE(std::memcmp(buf + bytes, p + bytes, 1), 0);
+  EXPECT_BUS_ERROR(0, 0, BusAccessKind::kNone);
+  EXPECT_EQ(std::memcmp(buf, this->p1_, bytes), 0);
+  EXPECT_NE(buf[bytes], this->p1_[bytes]);
 }
 
-TEST(Bus16Test, ReadForNotRegisterd) {
+TEST_F(Bus16Test, ReadForNotRegisterd) {
   // Setup
-  Bus16 bus(errorCallback);
   uint8_t buf[2 * 1024];
   uint_fast16_t addr = 0x100;
-  gaddr = 0;
-  gop = BusAccessKind::kNone;
   // Do
-  bus.read(addr, 1, buf);
+  this->bus_.read(addr, 1, buf);
   // Verify
-  EXPECT_EQ(gaddr, addr);
-  EXPECT_EQ(gop, BusAccessKind::kRead);
+  EXPECT_BUS_ERROR(1, addr, BusAccessKind::kRead);
 }
-TEST(Bus16Test, ReadForRegisterdOne) {
+TEST_F(Bus16Test, ReadForRegisterdOne) {
   // Setup
-  Bus16 bus(errorCallback);
-  Sram<1024> sram;
-  auto p = sram.data();
-  memset(p, 0xce, sram.size());
-  ASSERT_FALSE(sram.map(&bus, 0x8000));
+  ASSERT_FALSE(this->sram1_.map(&this->bus_, 0x8000));
   uint8_t buf[2 * 1024] = {0};
   uint_fast16_t addr = 0x8000;
   uint_fast16_t bytes = 0x100;
-  gaddr = 0;
-  gop = BusAccessKind::kNone;
   // Do
-  bus.read(addr, bytes, buf);
+  this->bus_.read(addr, bytes, buf);
   // Verify
-  EXPECT_EQ(gaddr, 0);
-  EXPECT_EQ(gop, BusAccessKind::kNone);
-  EXPECT_EQ(std::memcmp(buf, p, bytes), 0);
-  EXPECT_NE(std::memcmp(buf + bytes, p + bytes, 1), 0);
+  EXPECT_BUS_ERROR(0, 0, BusAccessKind::kNone);
+  EXPECT_EQ(std::memcmp(buf, this->p1_, bytes), 0);
+  EXPECT_EQ(buf[bytes], 0);
 }
-TEST(Bus16Test, ReadOver) {
+TEST_F(Bus16Test, ReadOver) {
   // Setup
-  Bus16 bus(errorCallback);
-  Sram<0x200> sram;
-  auto p = sram.data();
-  memset(p, 0xce, sram.size());
-  ASSERT_FALSE(sram.map(&bus, 0x100));
-  uint8_t buf[1 * 0x400] = {0};
+  ASSERT_FALSE(this->sram1_.map(&this->bus_, 0x100));
+  uint8_t buf[1 * 0x500] = {0};
   uint_fast16_t addr = 0x100;
-  uint_fast16_t bytes = 0x300;
-  gaddr = 0;
-  gop = BusAccessKind::kNone;
+  uint_fast16_t bytes = this->sram1_.size() + 2;
   // Do
-  bus.read(addr, bytes, buf);
+  this->bus_.read(addr, bytes, buf);
   // Verify
-  EXPECT_NE(gaddr, 0);
-  EXPECT_EQ(gop, BusAccessKind::kRead);
+  EXPECT_BUS_ERROR(1, addr + this->sram1_.size(), BusAccessKind::kRead);
 }
-TEST(Bus16Test, ReadAcrossTwoMap) {
+TEST_F(Bus16Test, ReadAcrossTwoMap) {
   // Setup
-  Bus16 bus(errorCallback);
-  Sram<0x200> sram;
-  Sram<0x200> sram2;
-  auto p1 = sram.data();
-  memset(p1, 0xce, sram.size());
-  auto p2 = sram2.data();
-  memset(p2, 0xa5, sram2.size());
-  ASSERT_FALSE(sram.map(&bus, 0x200));
-  ASSERT_FALSE(sram2.map(&bus, 0x400));
-  uint8_t buf[0x500] = {0};
-  uint_fast16_t addr = 0x200;
-  uint_fast16_t bytes = 0x400;
-  gaddr = 0;
-  gop = BusAccessKind::kNone;
+  ASSERT_FALSE(this->sram1_.map(&this->bus_, 0x400));
+  ASSERT_FALSE(this->sram2_.map(&this->bus_, 0x800));
+  uint8_t buf[0x400 * 2] = {0};
+  uint_fast16_t addr = 0x400;
+  uint_fast16_t bytes = this->sram1_.size() + 0x100;
   // Do
-  bus.read(addr, bytes, buf);
+  this->bus_.read(addr, bytes, buf);
   // Verify
-  EXPECT_EQ(gaddr, 0);
-  EXPECT_EQ(gop, BusAccessKind::kNone);
-  EXPECT_EQ(std::memcmp(buf, p1, 0x200), 0);
-  EXPECT_EQ(std::memcmp(buf + 0x200, p2, 0x200), 0);
-  EXPECT_EQ(buf[0x200 + 0x200], 0);
+  EXPECT_BUS_ERROR(0, 0, BusAccessKind::kNone);
+  EXPECT_EQ(std::memcmp(buf, this->p1_, this->sram1_.size()), 0);
+  EXPECT_EQ(std::memcmp(buf + this->sram1_.size(), this->p2_, 0x100), 0);
+  EXPECT_EQ(buf[this->sram1_.size() + 0x100], 0);
 }
 
-TEST(Bus16Test, WriteForNotRegisterd) {
+TEST_F(Bus16Test, WriteForNotRegisterd) {
   // Setup
-  Bus16 bus(errorCallback);
   const uint8_t buf[2] = {0};
   uint_fast16_t addr = 0x100;
-  gaddr = 0;
-  gop = BusAccessKind::kNone;
   // Do
-  bus.write(buf, sizeof(buf), addr);
+  this->bus_.write(buf, sizeof(buf), addr);
   // Verify: error
-  EXPECT_EQ(gaddr, addr);
-  EXPECT_EQ(gop, BusAccessKind::kWrite);
+  EXPECT_BUS_ERROR(1, addr, BusAccessKind::kWrite);
 }
-TEST(Bus16Test, WriteForRegisterdOne) {
+TEST_F(Bus16Test, WriteForRegisterdOne) {
   // Setup
-  Bus16 bus(errorCallback);
-  Sram<0x200> sram;
-  auto p = sram.data();
-  memset(p, 0xce, sram.size());
-  ASSERT_FALSE(sram.map(&bus, 0x8000));
+  ASSERT_FALSE(this->sram1_.map(&this->bus_, 0x8000));
   const uint8_t buf[0x200] = {0};
   uint_fast16_t addr = 0x8000;
   size_t bytes = 0x100;
-  gaddr = 0;
-  gop = BusAccessKind::kNone;
   // Do
-  bus.write(buf, bytes, addr);
-  // Verify: no error
-  EXPECT_EQ(gaddr, 0);
-  EXPECT_EQ(gop, BusAccessKind::kNone);
-  // Verify: written just the given bytes
-  EXPECT_EQ(std::memcmp(buf, p, bytes), 0);
-  EXPECT_EQ(p[bytes], 0xce);
+  this->bus_.write(buf, bytes, addr);
+  // Verify
+  EXPECT_BUS_ERROR(0, 0, BusAccessKind::kNone);
+  EXPECT_EQ(std::memcmp(buf, this->p1_, bytes), 0);
+  EXPECT_EQ(this->p1_[bytes], 0x01);
 }
-TEST(Bus16Test, WriteOver) {
+TEST_F(Bus16Test, WriteOver) {
   // Setup
-  Bus16 bus(errorCallback);
-  Sram<0x200> sram;
-  auto p = sram.data();
-  memset(p, 0xce, sram.size());
-  ASSERT_FALSE(sram.map(&bus, 0x100));
-  uint8_t buf[0x400] = {0};
+  ASSERT_FALSE(this->sram1_.map(&this->bus_, 0x100));
+  uint8_t buf[0x500] = {0};
   uint_fast16_t addr = 0x100;
-  size_t bytes = 0x300;
-  gaddr = 0;
-  gop = BusAccessKind::kNone;
+  size_t bytes = this->sram1_.size() + 2;
   // Do
-  bus.write(buf, bytes, addr);
-  // Verify: error
-  EXPECT_NE(gaddr, 0);
-  EXPECT_EQ(gop, BusAccessKind::kWrite);
+  this->bus_.write(buf, bytes, addr);
+  // Verify
+  EXPECT_BUS_ERROR(1, addr + this->sram1_.size(), BusAccessKind::kWrite);
 }
 
-TEST(Bus16Test, WriteReadForRegisterdOne) {
+TEST_F(Bus16Test, WriteReadForRegisterdOne) {
   // Setup
-  Bus16 bus(errorCallback);
-  Sram<0x400> sram;
-  auto p = sram.data();
-  memset(p, 0xce, sram.size());
-  ASSERT_FALSE(sram.map(&bus, 0x8000));
+  ASSERT_FALSE(this->sram1_.map(&this->bus_, 0x8000));
   const uint8_t buf[0x200] = {0};
   uint8_t buf2[0x200];
-  memset(buf2, 0x12, sizeof(buf));
+  memset(buf2, 0xce, sizeof(buf));
   uint_fast16_t addr = 0x8000;
   size_t bytes = 0x100;
-  gaddr = 0;
-  gop = BusAccessKind::kNone;
   // Do
-  bus.write(buf, bytes, addr);
-  // Verify: no error
-  EXPECT_EQ(gaddr, 0);
-  EXPECT_EQ(gop, BusAccessKind::kNone);
-  // Verify: written just the given bytes
-  EXPECT_EQ(std::memcmp(buf, p, bytes), 0);
-  EXPECT_EQ(p[bytes], 0xce);
+  this->bus_.write(buf, bytes, addr);
+  // Verify
+  EXPECT_BUS_ERROR(0, 0, BusAccessKind::kNone);
+  EXPECT_EQ(std::memcmp(buf, this->p1_, bytes), 0);
+  EXPECT_EQ(this->p1_[bytes], 0x01);
   // Do
-  bus.read(addr, bytes, buf2);
-  // Verify: no error
-  EXPECT_EQ(gaddr, 0);
-  EXPECT_EQ(gop, BusAccessKind::kNone);
-  // Verify: read just the given bytes
+  this->bus_.read(addr, bytes, buf2);
+  // Verify
+  EXPECT_BUS_ERROR(0, 0, BusAccessKind::kNone);
   EXPECT_EQ(std::memcmp(buf, buf2, bytes), 0);
-  EXPECT_EQ(buf2[bytes], 0x12);
+  EXPECT_EQ(buf2[bytes], 0xce);
 }
-TEST(Bus16Test, WriteReadAcrossTwoMap) {
+TEST_F(Bus16Test, WriteReadAcrossTwoMap) {
   // Setup
-  Bus16 bus(errorCallback);
-  Sram<0x200> sram;
-  Sram<0x300> sram2;
-  auto p1 = sram.data();
-  memset(p1, 0xce, sram.size());
-  auto p2 = sram2.data();
-  memset(p2, 0xa5, sram2.size());
-  ASSERT_FALSE(sram.map(&bus, 0x200));
-  ASSERT_FALSE(sram2.map(&bus, 0x400));
-  const uint8_t buf[0x500] = {0};
-  uint8_t buf2[0x500];
-  memset(buf2, 0x12, sizeof(buf));
-  uint_fast16_t addr = 0x200;
-  size_t bytes = 0x400;
-  gaddr = 0;
-  gop = BusAccessKind::kNone;
+  ASSERT_FALSE(this->sram1_.map(&this->bus_, 0x400));
+  ASSERT_FALSE(this->sram2_.map(&this->bus_, 0x800));
+  const uint8_t buf[0x500 + 2] = {0};
+  uint8_t buf2[0x500 + 2];
+  memset(buf2, 0xce, sizeof(buf));
+  uint_fast16_t addr = 0x400;
+  uint_fast16_t bytes = this->sram1_.size() + 0x100;
   // Do
-  bus.write(buf, bytes, addr);
+  this->bus_.write(buf, bytes, addr);
   // Verify: no error
-  EXPECT_EQ(gaddr, 0);
-  EXPECT_EQ(gop, BusAccessKind::kNone);
+  EXPECT_BUS_ERROR(0, 0, BusAccessKind::kNone);
   // Verify: written just the given bytes
-  EXPECT_EQ(std::memcmp(buf, p1, 0x200), 0);
-  EXPECT_EQ(std::memcmp(buf + 0x200, p2, 0x200), 0);
-  EXPECT_EQ(p2[0x200], 0xa5);
+  EXPECT_EQ(std::memcmp(buf, this->p1_, this->sram1_.size()), 0);
+  EXPECT_EQ(std::memcmp(buf + this->sram1_.size(), this->p2_, 0x100), 0);
+  EXPECT_EQ(this->p2_[0x100], 0x23);
   // Do
-  bus.read(addr, bytes, buf2);
-  // Verify: no error
-  EXPECT_EQ(gaddr, 0);
-  EXPECT_EQ(gop, BusAccessKind::kNone);
-  // Verify: read just the given bytes
+  this->bus_.read(addr, bytes, buf2);
+  // Verify
+  EXPECT_BUS_ERROR(0, 0, BusAccessKind::kNone);
   EXPECT_EQ(std::memcmp(buf, buf2, bytes), 0);
-  EXPECT_EQ(buf2[bytes], 0x12);
+  EXPECT_EQ(buf2[bytes], 0xce);
 }
 
-TEST(Bus16Test, WriteRead8) {
+TEST_F(Bus16Test, WriteRead8) {
   // Setup
-  Bus16 bus(errorCallback);
-  Sram<0x100> sram;
-  auto p = sram.data();
-  memset(p, 0xce, sram.size());
-  ASSERT_FALSE(sram.map(&bus, 0x8000));
+  ASSERT_FALSE(this->sram1_.map(&this->bus_, 0x8000));
   uint_fast16_t addr = 0x8000;
-  gaddr = 0;
-  gop = BusAccessKind::kNone;
   // Do
-  bus.write8(addr, 0x01);
+  this->bus_.write8(addr, 0x01);
+  auto ret = this->bus_.read8(addr);
   // Verify: no error
-  EXPECT_EQ(gaddr, 0);
-  EXPECT_EQ(gop, BusAccessKind::kNone);
-  // Do
-  auto ret = bus.read8(addr);
-  // Verify: no error
-  EXPECT_EQ(gaddr, 0);
-  EXPECT_EQ(gop, BusAccessKind::kNone);
+  EXPECT_BUS_ERROR(0, 0, BusAccessKind::kNone);
   EXPECT_EQ(ret, 0x01);
 }
-TEST(Bus16Test, WriteRead16) {
+TEST_F(Bus16Test, WriteRead16) {
   // Setup
-  Bus16 bus(errorCallback);
-  Sram<0x100> sram;
-  auto p = sram.data();
-  memset(p, 0xce, sram.size());
-  ASSERT_FALSE(sram.map(&bus, 0x8000));
+  ASSERT_FALSE(this->sram1_.map(&this->bus_, 0x8000));
   uint_fast16_t addr = 0x8000;
-  gaddr = 0;
-  gop = BusAccessKind::kNone;
   // Do
-  bus.write16(addr, 0x0123);
+  this->bus_.write16(addr, 0x0123);
+  auto ret = this->bus_.read16(addr);
   // Verify: no error
-  EXPECT_EQ(gaddr, 0);
-  EXPECT_EQ(gop, BusAccessKind::kNone);
-  // Do
-  auto ret = bus.read16(addr);
-  // Verify: no error
-  EXPECT_EQ(gaddr, 0);
-  EXPECT_EQ(gop, BusAccessKind::kNone);
+  EXPECT_BUS_ERROR(0, 0, BusAccessKind::kNone);
   EXPECT_EQ(ret, 0x0123);
 }
-TEST(Bus16Test, WriteRead32) {
+TEST_F(Bus16Test, WriteRead32) {
   // Setup
-  Bus16 bus(errorCallback);
-  Sram<0x100> sram;
-  auto p = sram.data();
-  memset(p, 0xce, sram.size());
-  ASSERT_FALSE(sram.map(&bus, 0x8000));
+  ASSERT_FALSE(this->sram1_.map(&this->bus_, 0x8000));
   uint_fast16_t addr = 0x8000;
-  gaddr = 0;
-  gop = BusAccessKind::kNone;
   // Do
-  bus.write32(addr, 0x01234567);
+  this->bus_.write32(addr, 0x01234567);
+  auto ret = this->bus_.read32(addr);
   // Verify: no error
-  EXPECT_EQ(gaddr, 0);
-  EXPECT_EQ(gop, BusAccessKind::kNone);
-  // Do
-  auto ret = bus.read32(addr);
-  // Verify: no error
-  EXPECT_EQ(gaddr, 0);
-  EXPECT_EQ(gop, BusAccessKind::kNone);
+  EXPECT_BUS_ERROR(0, 0, BusAccessKind::kNone);
   EXPECT_EQ(ret, 0x01234567);
 }
-TEST(Bus16Test, WriteRead64) {
+TEST_F(Bus16Test, WriteRead64) {
   // Setup
-  Bus16 bus(errorCallback);
-  Sram<0x100> sram;
-  auto p = sram.data();
-  memset(p, 0xce, sram.size());
-  ASSERT_FALSE(sram.map(&bus, 0x8000));
+  ASSERT_FALSE(this->sram1_.map(&this->bus_, 0x8000));
   uint_fast16_t addr = 0x8000;
-  gaddr = 0;
-  gop = BusAccessKind::kNone;
   // Do
-  bus.write64(addr, 0x0123456789abcdef);
+  this->bus_.write64(addr, 0x0123456789abcdef);
+  auto ret = this->bus_.read64(addr);
   // Verify: no error
-  EXPECT_EQ(gaddr, 0);
-  EXPECT_EQ(gop, BusAccessKind::kNone);
-  // Do
-  auto ret = bus.read64(addr);
-  // Verify: no error
-  EXPECT_EQ(gaddr, 0);
-  EXPECT_EQ(gop, BusAccessKind::kNone);
+  EXPECT_BUS_ERROR(0, 0, BusAccessKind::kNone);
   EXPECT_EQ(ret, 0x0123456789abcdef);
 }
+} // namespace nes_emu
